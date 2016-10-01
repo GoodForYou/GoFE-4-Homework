95c1d45753a999a8c32e21f9bdc48d2591b1d1e1
Artem Cherednychenko <artem.cherednychenko@gmail.com>
2016-10-01 15:06:21 +0300

    hw 05-6 js done

---
+++ js/homework05-6/script.js
@@ -0,0 +1,68 @@
+var startPauseButton = document.getElementById('start_pause');
+var clearButton = document.getElementById('clear');
+var timeResume = 0;
+var timeDiff = 0;
+var timerOn;
+
+function checkTime (x) {
+  if (x < 10) {
+    x = '0' + x;
+  }
+  return x;
+}
+
+function checkMilliseconds (x) {
+  if (x < 10) {
+    x = '00' + x;
+  } else if (x >= 10 && x < 100) {
+    x = '0' + x;
+  }
+  return x;
+}
+
+function timerStart () {
+  startPauseButton.innerHTML = 'Pause';
+
+  timeZero = new Date().getTime();
+
+  function currentTime () {
+    timeDiff = new Date().getTime() - timeZero + timeResume;
+    ms = parseInt(timeDiff % 1000);
+    s = parseInt((timeDiff/1000) % 60);
+    m = parseInt((timeDiff/(1000*60)) % 60);
+    h = parseInt((timeDiff/(1000*60*60)) % 24);
+
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    ms = checkMilliseconds(ms);
+
+    document.getElementById('timer').innerHTML = h + ":" + m + ":" + s + ":" + ms;
+}
+
+  timerOn = setInterval(currentTime, 1);
+}
+
+function timerPause () {
+  clearInterval(timerOn);
+  timeResume = timeDiff;
+  startPauseButton.innerHTML = 'Start';
+}
+
+function timer () {
+  if (startPauseButton.innerHTML === 'Start') {
+    timerStart();
+  } else {
+    timerPause();
+  }
+}
+
+function clear () {
+    clearInterval(timerOn);
+    startPauseButton.innerHTML = 'Start';
+    timeResume = 0;
+    document.getElementById('timer').innerHTML = "00:00:00:000";
+}
+
+startPauseButton.addEventListener('click', timer);
+clearButton.addEventListener('click', clear);
