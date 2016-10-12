965c7a5a501653ad6e1e7af12a03673f7a047010
Artem Cherednychenko <artem.cherednychenko@gmail.com>
2016-10-13 00:19:07 +0300

    hw 9-10 done

---
+++ js/homework09-10/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  $('.mySelectBoxClass').customSelect();
+
+  $('.drop-down-menu').hover(
+    function() {
+      $(this).children('.submenu').show();
+    },
+    function() {
+      $(this).children('.submenu').hide();
+    }
+  )
+});
